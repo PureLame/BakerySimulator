c97d148f19f82154725ee5deec5cafc62d15857d
PureLame <pure.lame.01@gmail.com>
2021-05-31 19:47:41 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,6 +10,23 @@ namespace ConsoleApp5
     {
         static void Main(string[] args)
         {
+            int money;
+            int eatCount;
+            int eatPrice = 10;
+            bool enoughMoney;
+
+            Console.WriteLine("Добро пожаловать в пекарню! Сегодня еда по " + eatPrice + " монет.");
+            Console.Write("Сколько у вас золота: ");
+            money = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Сколько еды вам нужно: ");
+            eatCount = Convert.ToInt32(Console.ReadLine());
+
+            enoughMoney = money >= eatCount * eatPrice;
+
+            eatCount *= Convert.ToInt32(enoughMoney);
+            money -= eatCount * eatPrice;
+
+            Console.WriteLine("У вас в сумке - " + eatCount + " еды и " + money + " монет.");
         }
     }
 }
